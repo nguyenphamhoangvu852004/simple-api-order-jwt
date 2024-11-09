733143747b51f6513a71d15304dcddb2e56d4781
nguyenphamhoangvu852004 <nguyenphamhoangvu852004@gmail.com>
2024-11-09 18:47:54 +0700

    append get user profile by id feature

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Put, UseGuards, Request } from '@nestjs/common';
+import { Body, Controller, Put, UseGuards, Request, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { JwtAuthGuard } from '../auth/passport/jwt-auth.guard';
 import { UpdateUserInputDto, UpdateUserOutputDto } from './dto/update.user.dto';
@@ -8,6 +8,7 @@ import {
   ApiOperation,
   ApiResponse,
 } from '@nestjs/swagger';
+import { GetUserByIdOutputDto } from './dto/get.user.by.id.dto';
 
 @Controller('users')
 export class UsersController {
@@ -31,5 +32,19 @@ export class UsersController {
     const userId = req.user.userId; // lấy userId từ accessToken đã được giải mã
 
     return await this.usersService.updateUser(userId, updateUserDto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('')
+  @ApiBearerAuth() // Để Swagger biết endpoint này cần xác thực bằng JWT
+  @ApiOperation({ summary: 'Get profile user by userId' }) // Tóm tắt chức năng của endpoint
+  @ApiResponse({
+    status: 200,
+    description: 'Get User profile successfully',
+    type: GetUserByIdOutputDto, // Kiểu dữ liệu trả về
+  })
+  async getUserById(@Request() req): Promise<GetUserByIdOutputDto> {
+    const userId: number = req.user.userId;
+    return await this.usersService.getUserProfileById(userId);
   }
 }
