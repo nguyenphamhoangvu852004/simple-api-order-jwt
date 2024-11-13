53dd59c896bb02ce75bfde038000d607066f9e0a
nguyenphamhoangvu852004 <nguyenphamhoangvu852004@gmail.com>
2024-11-13 20:33:26 +0700

    custom jwt auth guard

--- src/auth/passport/jwt-auth.guard.ts
+++ src/auth/passport/jwt-auth.guard.ts
@@ -7,6 +7,15 @@ import { AuthGuard } from '@nestjs/passport';
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard('jwt') {
+  canActivate(context) {
+    // Thực hiện xác thực JWT
+    return super.canActivate(context);
+  }
+
+  handleRequest(err, user) {
+    // Gán thông tin người dùng vào request
+    return user;
+  }
   // canActivate(context: ExecutionContext) {
   //   // Add your custom authentication logic here
   //   // for example, call super.logIn(request) to establish a session.
